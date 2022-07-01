e6b69e3a2828946d489b65f178c1215d2e541314
Christian Ipanaque <chris.ipanaque@gmail.com>
2022-07-01 13:10:39 -0700

    add meta tag verification

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="PauTD2R5Nx08EXP-UuVDWL1jvgydl5YX9rXtrUjl8-o"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
